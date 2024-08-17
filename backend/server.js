2a7cd0ed72d58b8da47fd1a564ed1ee771d18750
Harry Mcdonald <harryquirk96@icloud.com>
2024-08-17 18:38:50 +0100

    app.options

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // For parsing application/json
 
+// Handle preflight requests for all routes
+app.options('*', cors());
+
 // MongoDB URI
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri);
